0b943adc75eba349c815b645ead5e3a98387ebb2
Juan D. Gaitán <jdgaitanf@unal.edu.co>
2021-05-01 22:13:52 -0500

    first commit

---
+++ app/src/main/java/com/example/offsongs/Common/Common.java
@@ -0,0 +1,14 @@
+package com.example.offsongs.Common;
+
+public class Common {
+    // The EditText String will be
+    // stored in this variable
+    // when MINIMIZE or MAXIMIZE
+    // button is pressed
+    public static String currentDesc = "";
+
+    // The EditText String will be
+    // stored in this variable
+    // when SAVE button is pressed
+    public static String savedDesc = "";
+}
